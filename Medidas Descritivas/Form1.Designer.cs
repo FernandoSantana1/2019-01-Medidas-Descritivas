559cc0ade614f1bf77f6e69bbd0e1b633d810181
Fernando Santana <fernando.santana10000@gmail.com>
2022-03-21 23:15:32 -0300

    commit

---
+++ Medidas Descritivas/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace Medidas_Descritivas
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblMedia = new System.Windows.Forms.Label();
+            this.lblMediana = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lblPopula = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.lblAmostral = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.lblPadrao = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.lblCV = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.lblCVH = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Calibri Light", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.numericUpDown1.Location = new System.Drawing.Point(80, 12);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            100000,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(162, 31);
+            this.numericUpDown1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(84, 49);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 44);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Adicionar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(9, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(63, 23);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Média:";
+            // 
+            // lblMedia
+            // 
+            this.lblMedia.AutoSize = true;
+            this.lblMedia.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMedia.Location = new System.Drawing.Point(239, 107);
+            this.lblMedia.Name = "lblMedia";
+            this.lblMedia.Size = new System.Drawing.Size(20, 23);
+            this.lblMedia.TabIndex = 7;
+            this.lblMedia.Text = "0";
+            // 
+            // lblMediana
+            // 
+            this.lblMediana.AutoSize = true;
+            this.lblMediana.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMediana.Location = new System.Drawing.Point(239, 141);
+            this.lblMediana.Name = "lblMediana";
+            this.lblMediana.Size = new System.Drawing.Size(20, 23);
+            this.lblMediana.TabIndex = 10;
+            this.lblMediana.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(9, 141);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 23);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "Mediana:";
+            // 
+            // lblPopula
+            // 
+            this.lblPopula.AutoSize = true;
+            this.lblPopula.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPopula.Location = new System.Drawing.Point(239, 174);
+            this.lblPopula.Name = "lblPopula";
+            this.lblPopula.Size = new System.Drawing.Size(20, 23);
+            this.lblPopula.TabIndex = 17;
+            this.lblPopula.Text = "0";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(9, 174);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(187, 23);
+            this.label4.TabIndex = 16;
+            this.label4.Text = "Variância populacional:";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(165, 49);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(77, 44);
+            this.button2.TabIndex = 18;
+            this.button2.Text = "Calcular";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // lblAmostral
+            // 
+            this.lblAmostral.AutoSize = true;
+            this.lblAmostral.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAmostral.Location = new System.Drawing.Point(239, 207);
+            this.lblAmostral.Name = "lblAmostral";
+            this.lblAmostral.Size = new System.Drawing.Size(20, 23);
+            this.lblAmostral.TabIndex = 21;
+            this.lblAmostral.Text = "0";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(9, 207);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(157, 23);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Variância amostral:";
+            // 
+            // lblPadrao
+            // 
+            this.lblPadrao.AutoSize = true;
+            this.lblPadrao.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPadrao.Location = new System.Drawing.Point(239, 238);
+            this.lblPadrao.Name = "lblPadrao";
+            this.lblPadrao.Size = new System.Drawing.Size(20, 23);
+            this.lblPadrao.TabIndex = 23;
+            this.lblPadrao.Text = "0";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(9, 238);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(212, 23);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "Desvio padrão (Amostral):";
+            // 
+            // lblCV
+            // 
+            this.lblCV.AutoSize = true;
+            this.lblCV.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCV.Location = new System.Drawing.Point(239, 271);
+            this.lblCV.Name = "lblCV";
+            this.lblCV.Size = new System.Drawing.Size(20, 23);
+            this.lblCV.TabIndex = 25;
+            this.lblCV.Text = "0";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(9, 271);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(193, 23);
+            this.label8.TabIndex = 24;
+            this.label8.Text = "Coeficiente de Variação:";
+            // 
+            // lblCVH
+            // 
+            this.lblCVH.AutoSize = true;
+            this.lblCVH.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCVH.Location = new System.Drawing.Point(116, 309);
+            this.lblCVH.Name = "lblCVH";
+            this.lblCVH.Size = new System.Drawing.Size(0, 23);
+            this.lblCVH.TabIndex = 26;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(334, 341);
+            this.Controls.Add(this.lblCVH);
+            this.Controls.Add(this.lblCV);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.lblPadrao);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.lblAmostral);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.lblPopula);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblMediana);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblMedia);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Medidas Descritivas";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblMedia;
+        private System.Windows.Forms.Label lblMediana;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblPopula;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label lblAmostral;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label lblPadrao;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblCV;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label lblCVH;
+    }
+}
+
---
+++ Medidas Descritivas/Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Medidas_Descritivas
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        
+        List<double> teste = new List<double>(); double soma_; double desPadrao; double CV;
+        double var; List<double> lista = new List<double>(); double aux; int y;
+        double soma;  double media; public double mediana; double valor;
+        //double[] items = new double[4];
+        List<double> itens = new List<double>();
+        public List<double> NumerosMedia = new List<double>();
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            ////////////////////   MÉDIA  ////////////////////
+            NumerosMedia.Add(Convert.ToDouble(numericUpDown1.Value));
+            soma = 0;
+            for (int i = 0; i < NumerosMedia.Count; i++)
+            {
+                soma = soma + NumerosMedia[i];
+            }
+            media = (soma / NumerosMedia.Count);
+            lblMedia.Text = Convert.ToString(Math.Round(media, 2));
+
+            ////////////////////   MEDIANA  ////////////////////
+
+            lista.Add(valor);
+            CalcularMediana(Convert.ToDouble(numericUpDown1.Value));
+            void CalcularMediana(double valor)
+            {
+                var Mediana = 0.0;
+                itens.Add(valor);
+                var qntItens = itens.Count; //pega a quantidade 
+                itens.Sort(); //ordena os valores
+                if (qntItens % 2 == 0) // verifica se a quantidade eh par
+                {
+                    //Calculo da mediana (A + B / 2) = ?
+                    var primeiroValor = itens[(itens.Count / 2) - 1]; 
+                    var segundoValor = itens[(itens.Count / 2)];
+                    Mediana = (primeiroValor + segundoValor) / 2.0;
+                }
+                if (qntItens % 2 == 1)// verifica se a quantidade eh impar
+                {
+                    Mediana = itens[(itens.Count / 2)];
+                }
+                lblMediana.Text = Mediana.ToString();
+            }
+        }
+        
+
+
+
+
+
+
+
+
+
+
+
+        private void Amostral_Padrao(int A_P)
+        {
+            aux = 0;y = 0;var = 0;teste.Clear();soma_ = 0;
+            ////////////////////   VARIAÇÃO AMOSTRAL  ////////////////////
+            while (y < lista.Count)
+            {
+                valor = Convert.ToDouble(numericUpDown1.Value);
+                var = lista[y] - media;
+                var = var * var; //resultado ao quadrado
+                teste.Add(var);
+                y++;
+            }
+            for (int i = 0; i < teste.Count; i++) //somar
+            {
+                soma_ += teste[i];
+            }
+            aux = soma_ / (lista.Count - A_P);
+            if (A_P == 0)//populacional
+            {
+                lblPopula.Text = Convert.ToString(Math.Round(aux, 2));
+            }
+            if (A_P == 1)//amostral
+            {
+                lblAmostral.Text = Convert.ToString(Math.Round(aux, 2));
+                desPadrao = Math.Round(Math.Sqrt(aux),2); // Raiz / Desvio Padrao 
+                lblPadrao.Text = desPadrao.ToString();   // Desvio Padrao
+                CV = Math.Round((desPadrao / media * 100), 2);
+                lblCV.Text = CV + "%";
+                if(CV <= 30)
+                {
+                    lblCVH.ForeColor = Color.Green;
+                    lblCVH.Text = "Homogêneo";
+                }
+                else
+                {
+                    lblCVH.ForeColor = Color.Red;
+                    lblCVH.Text = "Heterogêneo";
+                }
+            }
+        }
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            Amostral_Padrao(0);
+            Amostral_Padrao(1);
+        }
+    }
+}
